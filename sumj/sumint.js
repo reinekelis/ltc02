af554a6a951f35360776642f6c9c88145d93fb44
Reinike <Reinike@MacBook-Tanya-Smehnova.local>
2019-03-18 16:46:40 +0100

    js with 2 parameters

--- sumj/sumint.js
+++ sumj/sumint.js
@@ -3,9 +3,7 @@ function sumint(a,b)
     return (parseInt(a) + parseInt(b));
 }
 
-let param1 =4575;
-let param2 =43253;
+let args = process.argv.slice(2);
+let totalsum = sumint(...args);
 
-let totalsum = sumint(param1, param2);
-
-console.log( param1+ " + "+ param2 + " = " +totalsum);
+console.log( "= " +totalsum);
