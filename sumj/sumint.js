5f1adfada6324eaea4e85852ef9b57b626cdebd9
Reinike <Reinike@MacBook-Tanya-Smehnova.local>
2019-03-18 16:53:43 +0100

    with error message

--- sumj/sumint.js
+++ sumj/sumint.js
@@ -1,8 +1,10 @@
-function sumint(a,b)
-{
+function sumint(a,b){
     return (parseInt(a) + parseInt(b));
 }
 
+if (process.argv.length !==4){
+    throw new Error('run script with 2 integer numbers as arguments');
+}
 let args = process.argv.slice(2);
 let totalsum = sumint(...args);
 
